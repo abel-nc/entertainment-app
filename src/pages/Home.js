679bb718fe8b8d261099fc55c1939f9bec3238eb
Abel Navarro <112418726+abel-nc@users.noreply.github.com>
2022-11-19 15:03:58 -0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import TopRated from '../components/Home/TopRated'
 import Trending from '../components/Home/Trending'
-import Upcoming from '../components/Home/Upcoming'
+import Popular from '../components/Home/Popular'
 
 const Home = () => {
     return (
         <section>
             {/* MOVIES */}
             <Trending type={'movie'} />
-            <Upcoming type={'movie'} />
+            <Popular type={'movie'} />
             <TopRated type={'movie'} />
             {/* TV SERIES */}
             <Trending type={'tv'} />
+            <Popular type={'tv'} />
             <TopRated type={'tv'} />
         </section>
     )
